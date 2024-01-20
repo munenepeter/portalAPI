24f3db36f60020a1ef531eea7477373cf6bbf98d
munenepeter <peternjega7@gmail.com>
2024-01-20 15:03:49 +0300

    chore: add vue to discoverable paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,8 @@ import forms from '@tailwindcss/forms';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
+        './resources/js/*.vue',
+        './resources/js/**/*.vue',
         './resources/views/**/*.blade.php',
     ],
 
