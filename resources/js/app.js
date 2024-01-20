6362daa31405b1f55855b7d9516fcc6a9f432f2c
munenepeter <peternjega7@gmail.com>
2024-01-20 14:32:30 +0300

    feat: create vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,17 @@
 import './bootstrap';
+
+
+
+import {
+    createApp
+} from 'vue';
+
+import router from './router'
+
+import Index from './components/Index.vue';
+
+createApp({
+    components: {
+        Index
+    }
+}).use(router).mount('#app')
