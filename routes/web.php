ef6660b34fd904e8444a14931d7c18b698db67c0
munenepeter <peternjega7@gmail.com>
2024-01-20 14:32:12 +0300

    chore: move to using a single file

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-
-Route::view('/{any}', 'dashboard')->middleware(['auth'])->where('any','.*');
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/{any}', 'app')->where('any','.*');
